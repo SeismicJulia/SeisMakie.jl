9d6245d0b95f0a8251f97547c862246a06acd136
Firas <firas_alchalabi@hotmail.com>
2024-01-22 19:16:04 -0700

    Initial Upload

--- src/SeisMakie.jl
+++ src/SeisMakie.jl
@@ -1,5 +1,11 @@
 module SeisMakie
 
-# Write your package code here.
+    # Write your package code here.
+    using Makie
+
+    include("WiggleRecipe.jl")
+    include("ImageRecipe.jl")
+
+    export wiggleplot
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using SeisMakie
 using Test
+using SeisProcessing
 
 @testset "SeisMakie.jl" begin
-    # Write your tests here.
+    # Write your tests here
 end

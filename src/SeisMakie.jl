a59b6029bfc5afa7c9e006cbe3e9a8d71bf64ac6
Firas <firas_alchalabi@hotmail.com>
2024-02-14 19:03:35 -0700

    Changed package structure

--- src/SeisMakie.jl
+++ src/SeisMakie.jl
@@ -1,11 +1,32 @@
+
 module SeisMakie
 
     # Write your package code here.
     using Makie
+    using Statistics
+    using FFTW
 
-    include("WiggleRecipe.jl")
-    include("ImageRecipe.jl")
+    include("Recipes/SeisWiggleRecipe.jl")
+    include("Recipes/SeisColorRecipe.jl")
+    include("Recipes/SeisOverlayRecipe.jl")
 
-    export wiggleplot
+    include("SeisWiggle.jl")
+    include("SeisOverlay.jl")
+    include("SeisColor.jl")
+    include("SeisDifference.jl")
+
+
+    include("SeisPlotFK.jl")
+    include("SeisPlotTX.jl")
+
+    include("Util.jl")
+
+    export SeisWiggle
+    export SeisColor
+    export SeisOverlay
+
+    export SeisDifference
+    export SeisPlotTX
+    export SeisPlotFK
 
 end

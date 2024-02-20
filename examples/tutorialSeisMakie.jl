18e5237afec0c76b1c2924b4b696d2c799c62c4c
Firas <firas_alchalabi@hotmail.com>
2024-02-20 02:35:00 -0700

    Updated examples to reflect changes

--- examples/tutorialSeisMakie.jl
+++ examples/tutorialSeisMakie.jl
@@ -36,26 +36,29 @@ begin
 	nothing
 end
 
+# ╔═╡ c25286cd-82b5-4725-96cc-4ab6ed0aa121
+begin
+	f1, ax1 = SeisPlotTX(D, style="wiggle")
+	f1
+end
+
 # ╔═╡ 26992493-fe50-4f59-8141-9ea9e8606516
 
 
 # ╔═╡ abe64b2a-f192-4e3b-ba3e-f26286b1a38d
 md"""
-## SeisWiggle
+## SeisPlotTX
 """
 
-# ╔═╡ c25286cd-82b5-4725-96cc-4ab6ed0aa121
-begin
-	f1, ax1 = SeisWiggle(D)
-	f1
-end
+# ╔═╡ 0c3fcf92-8924-49ac-b487-a9dc372b2739
+
 
 # ╔═╡ 25c72bf7-28ce-4f6b-b4c4-24be2f51a5f9
 begin
 	function start_and_interval()
 		ox, dx = 1000, 3
 		oy, dy = 500, 5
-		f, ax = SeisWiggle(D, ox=ox, dx=dx, oy=oy, dy=dy)
+		f, ax = SeisPlotTX(D, ox=ox, dx=dx, oy=oy, dy=dy, style="wiggle")
 		f
 	end
 	start_and_interval()
@@ -73,7 +76,7 @@ begin
 		gx = gx[p]
 		d = d[:, p]
 	
-		f, ax = SeisWiggle(d, gx=gx)
+		f, ax = SeisPlotTX(d, gx=gx, style="wiggle")
 		f
 	end
 	real_coords()
@@ -82,7 +85,7 @@ end
 # ╔═╡ 38fcc0c0-aaaa-4777-b67c-9cc5c9289a91
 begin
 	function excursion()
-		f, ax = SeisWiggle(D, xcur=5)
+		f, ax = SeisPlotTX(D, xcur=5, style="wiggle")
 		f
 	end
 	excursion()
@@ -91,7 +94,7 @@ end
 # ╔═╡ 9a61b3d7-3a77-417d-b60b-7172050167e4
 begin
 	function trace_increment()
-		f, ax = SeisWiggle(D, wiggle_trace_increment=4)
+		f, ax = SeisPlotTX(D, wiggle_trace_increment=4, style="wiggle")
 		f
 	end
 	trace_increment()
@@ -100,7 +103,7 @@ end
 # ╔═╡ e6f38a8d-3db9-47ed-801e-2f434d2ef70c
 begin
 	function formatting()
-		f, ax = SeisWiggle(D, wiggle_trace_increment=4, wiggle_line_color=:orange, wiggle_fill_color=:purple)
+		f, ax = SeisPlotTX(D, wiggle_trace_increment=4, wiggle_line_color=:orange, wiggle_fill_color=:purple, style="wiggle")
 		f
 	end
 	formatting()
@@ -108,13 +111,13 @@ end
 
 # ╔═╡ a66db96a-a6f1-4628-8a89-d8b2744eb3b5
 md"""
-## SeisColor
+### SeisColor
 """
 
 # ╔═╡ 0a63424d-ab08-4dd4-bff8-5239ec312733
 begin
 	function base_color()
-		f, ax = SeisColor(D)
+		f, ax = SeisPlotTX(D, style="color")
 		f
 	end
 	base_color()
@@ -123,7 +126,7 @@ end
 # ╔═╡ a546c9bc-7d20-4636-850f-f900194e97e8
 begin
 	function with_pclip()
-		f, ax = SeisColor(D, pclip=95)
+		f, ax = SeisPlotTX(D, pclip=95, style="color")
 		f
 	end
 	dx = 1
@@ -133,7 +136,7 @@ end
 # ╔═╡ 9aff572e-683c-4085-878f-8c87c046db6f
 begin
 	function with_vmin_vmax()
-		f, ax = SeisColor(D, vmin=-0.1, vmax=0.4)
+		f, ax = SeisPlotTX(D, vmin=-0.1, vmax=0.4, style="color")
 		f
 	end
 	with_vmin_vmax()
@@ -142,7 +145,7 @@ end
 # ╔═╡ 21cb4890-628a-4331-a163-77fd8b3c9e05
 begin
 	function with_different_colormap()
-		f, ax = SeisColor(D, colormap=:seismic)
+		f, ax = SeisPlotTX(D, cmap=:seismic, style="color")
 		f
 	end
 	with_different_colormap()
@@ -150,14 +153,14 @@ end
 
 # ╔═╡ 526287c9-8d28-4357-8313-6f7ba2f2f49b
 md"""
-## SeisOverlay
+### SeisOverlay
 You can use a combination of the arguments supplied to SeisWiggle and SeisColor
 """
 
 # ╔═╡ 312a0197-d66c-436d-9452-7c70e4cfaded
 begin
 	function base_overlay()
-		f, ax = SeisOverlay(D)
+		f, ax = SeisPlotTX(D, style="overlay")
 		f
 	end
 	base_overlay()
@@ -166,25 +169,10 @@ end
 # ╔═╡ 483422b0-666b-4600-a96b-9dd84ca19298
 begin
 	function overlay_with_args()
-		f, ax = SeisOverlay(D, ox=20, dx=10, oy=500, dy=3, colormap=:seismic, vmin=-0.2, vmax=0.3)
+		f, ax = SeisPlotTX(D, ox=20, dx=10, oy=500, dy=3, cmap=:seismic, vmin=-0.2, vmax=0.3, style="overlay")
 		f
 	end
 	overlay_with_args()
-end
-
-# ╔═╡ 2b231e00-8952-4bfc-b26e-79abcc6258e9
-md"""
-## SeisPlotTX
-Based on request. Pass in the arguments you want along with a `style` keyword. `style` can be one of "wiggle", "overlay", "color".
-"""
-
-# ╔═╡ 6ccaf31a-caa8-4118-861d-9dd960b28d73
-begin 
-	function seisplottx_demo()
-		f, ax = SeisPlotTX(D, style="overlay")
-		f
-	end
-	seisplottx_demo()
 end
 
 # ╔═╡ ee40ceea-7d3f-4578-902d-d1518f4c7c84
@@ -211,7 +199,7 @@ You can edit axes as shown below. For more info, go to: https://docs.makie.org/s
 begin 
 	function modifying_axis()
 		# All the functions mentioned above return an axis that can be edited
-		f, ax = SeisOverlay(D, pclip=97)
+		f, ax = SeisPlotTX(D, pclip=97)
 		ax.title = "Overlay Plot (SeisMakie)"
 		ax.xlabel = "Trace Number"
 		ax.ylabel = "Time (s)"
@@ -233,9 +221,9 @@ begin
 		# by the first function. The position of the axis on the figure is indexed as 
 		# shown below
 		f = Figure()
-		_, wiggle_ax = SeisWiggle(D, fig=f[1,1], wiggle_trace_increment=4)
-		_, color_ax = SeisColor(D, fig=f[2, 1])
-		_, overlay_ax = SeisOverlay(D, fig=f[1,2], wiggle_trace_increment=4)
+		_, wiggle_ax = SeisPlotTX(D, fig=f[1,1], wiggle_trace_increment=4, style="wiggle")
+		_, color_ax = SeisPlotTX(D, fig=f[2, 1], style="color")
+		_, overlay_ax = SeisPlotTX(D, fig=f[1,2], wiggle_trace_increment=4, style="overlay")
 
 		wiggle_ax.title = "Wiggle"
 		color_ax.title = "Color"
@@ -258,20 +246,67 @@ begin
 	end
 end
 
-# ╔═╡ f2ac7e44-7a07-4956-9f22-526af10d1fe0
+# ╔═╡ 55151c58-ce42-4473-8bee-6ae501b5a891
+md"""
+## SeisPlotAmplitude
+"""
 
+# ╔═╡ f2ac7e44-7a07-4956-9f22-526af10d1fe0
+begin
+	function spamplitude()
+		f, ax = SeisPlotAmplitude(D)
+		f
+	end
+	spamplitude()
+end
 
 # ╔═╡ 1fcb8085-02f2-4907-999a-19db73ce4563
+begin
+	function spamplitude_multiple()
+		f, ax = SeisPlotAmplitude(D, label="D")
+		SeisPlotAmplitude(D1, ax=ax, label="D1")
+		SeisPlotAmplitude(D2, ax=ax, label="D2")
+		axislegend(ax, pos = :tr) # Top right position
+		#Legend(f[1, 2], ax)
+		f
+	end
+	spamplitude_multiple()
+end
 
+# ╔═╡ 9ceb6a8a-9235-49af-94ae-aa762e96aff0
+md"""
+## SeisDifference
+"""
+
+# ╔═╡ 7bad9eb6-9201-4642-b147-193fbb5702fd
+begin 
+	function horizontal_diff()
+		f, ax = SeisDifference(D1, D2)
+		f
+	end
+	horizontal_diff()
+
+end
+
+# ╔═╡ ab183ae7-8e20-4413-b4f4-68000038974c
+begin 
+	function vertical_diff()
+		f, ax1, ax2, ax3 = SeisDifference(D1, D2, horizontal=false)
+		f
+	end
+	vertical_diff()
+
+end
 
 # ╔═╡ Cell order:
 # ╠═93296b66-bca6-11ee-0787-99798c15a928
+# ╠═c25286cd-82b5-4725-96cc-4ab6ed0aa121
 # ╠═90077fbb-2384-4d12-b51a-f52758049e49
 # ╠═62e31cae-8deb-49a5-a6a1-54a772b48ff6
 # ╠═e12751b3-0c8d-4655-b606-ae94d670b0d0
 # ╠═26992493-fe50-4f59-8141-9ea9e8606516
 # ╠═abe64b2a-f192-4e3b-ba3e-f26286b1a38d
-# ╠═c25286cd-82b5-4725-96cc-4ab6ed0aa121
+# ╠═0c3fcf92-8924-49ac-b487-a9dc372b2739
 # ╠═25c72bf7-28ce-4f6b-b4c4-24be2f51a5f9
 # ╠═13980acc-8aa1-4d87-ab7e-c1a599e0483d
 # ╠═38fcc0c0-aaaa-4777-b67c-9cc5c9289a91
@@ -285,8 +320,6 @@ end
 # ╠═526287c9-8d28-4357-8313-6f7ba2f2f49b
 # ╠═312a0197-d66c-436d-9452-7c70e4cfaded
 # ╠═483422b0-666b-4600-a96b-9dd84ca19298
-# ╠═2b231e00-8952-4bfc-b26e-79abcc6258e9
-# ╠═6ccaf31a-caa8-4118-861d-9dd960b28d73
 # ╠═ee40ceea-7d3f-4578-902d-d1518f4c7c84
 # ╠═e12d35e5-4ea6-42f4-a396-92e4d4ebc5c7
 # ╠═3947a6c1-4bbe-4ee0-a3e6-114e481dc49c
@@ -295,5 +328,9 @@ end
 # ╠═cf728878-fefb-4d46-a52e-1d0820248d35
 # ╠═01db2622-4b44-492d-a0f6-838018727e75
 # ╠═8dcce330-fad7-4dde-86c9-f3c732089278
+# ╠═55151c58-ce42-4473-8bee-6ae501b5a891
 # ╠═f2ac7e44-7a07-4956-9f22-526af10d1fe0
 # ╠═1fcb8085-02f2-4907-999a-19db73ce4563
+# ╠═9ceb6a8a-9235-49af-94ae-aa762e96aff0
+# ╠═7bad9eb6-9201-4642-b147-193fbb5702fd
+# ╠═ab183ae7-8e20-4413-b4f4-68000038974c

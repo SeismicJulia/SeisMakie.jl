1641c2baf9eca5631532f5a64cefdfdb60f18537
Firas <firas_alchalabi@hotmail.com>
2024-03-05 18:57:14 -0700

    Updated SeisPlotTX documentation

--- src/SeisPlotTX.jl
+++ src/SeisPlotTX.jl
@@ -11,7 +11,8 @@ Plot time-space, 2D seismic data `d` with image, wiggles or overlay.
 # Keyword Arguments:
 - `fig=nothing`: the figure we want to plot on. If not supplied, one will be created and returned.
 
-- `gx::Vector{<:Real}=nothing`: the real coordinates of the seismometers corresponding to the traces in d
+- `gx::Vector{<:Real}=nothing`: the real coordinates of the seismometers corresponding to the traces in d. Only 
+                                supported with style="wiggles"
 - `ox=0`: first point of x-axis.
 - `dx=1`: increment of x-axis.
 - `oy=0`: first point of y-axis.

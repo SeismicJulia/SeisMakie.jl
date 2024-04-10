0ff4f2c1fcacfac7a515520e5733cb8ad04021b1
Firas <firas_alchalabi@hotmail.com>
2024-04-10 08:32:48 -0600

    Updated tests to new wrapper/recipe names

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using SeisMakie
 using SeisProcessing
 using Random
 using Test
+using Makie
 
 @testset "SeisMakie.jl" begin
     
@@ -16,14 +17,14 @@ using Test
         p = (randsubseq(gx, 0.85) .÷ dx) .+ 1
         gx = gx[p]
 
-        f, ax, wp = seiswiggle(d[:, p], gx=gx, dy=dy, xcur=2, wiggle_trace_increment=4)
+        f, ax, wp = seiswiggleplot(d[:, p], gx=gx, dy=dy, xcur=2, wiggle_trace_increment=4)
 
         @test true
     end
 
     @testset "SeisImageRecipe" begin
 
-        f, ax, img = seisimage(d, dx=dx, dy=dy, pclip=97, cmap=:viridis)
+        f, ax, img = seisimageplot(d, dx=dx, dy=dy, pclip=97, cmap=:viridis)
 
         @test true
 
@@ -31,7 +32,7 @@ using Test
 
     @testset "SeisOverlayRecipe" begin
 
-        f, ax, ov = seisoverlay(d, dx=dx, dy=dy, xcur=2, wiggle_trace_increment=4, pclip=97, cmap=:viridis)
+        f, ax, ov = seisoverlayplot(d, dx=dx, dy=dy, xcur=2, wiggle_trace_increment=4, pclip=97, cmap=:viridis)
 
         @test true
 
@@ -39,14 +40,18 @@ using Test
 
     @testset "SeisFKRecipe" begin
         
-        f, ax, fk = seisfk(d, dx=dx, dy=dy, pclip=97, fmax=90)
+        f, ax, fk = seisfkplot(d, dx=dx, dy=dy, pclip=97, fmax=90)
 
         @test true
     end
 
     @testset "SeisAmplitudeRecipe" begin
         
-        f, ax, amp = seisamplitude(d, dy=dy, fmax=90)
+        f, ax, amp = seisamplitudeplot(d, dy=dy, fmax=90)
+
+        f = Figure()
+        ax = Axis(f[1,1])
+        amp = seisamplitudeplot!(ax, d)
 
         @test true
     end
@@ -56,24 +61,32 @@ using Test
         # create a random permutation vector to sample gx and d
         p = (randsubseq(gx, 0.85) .÷ dx) .+ 1
         gx = gx[p]
-        f, ax = SeisPlotTX(d[:, p], gx=gx, dy=dy, xcur=2, wiggle_trace_increment=4, style="wiggle")
+        f, ax = seisPlotTX(d[:, p], gx=gx, dy=dy, xcur=2, wiggle_trace_increment=4, style="wiggle")
         @test true
-        f, ax = SeisPlotTX(d, dx=dx, dy=dy, pclip=97, cmap=:viridis, style="image")
+        f, ax = seisPlotTX(d, dx=dx, dy=dy, pclip=97, cmap=:viridis, style="image")
         @test true
-        f, ax = SeisPlotTX(d, dx=dx, dy=dy, xcur=2, wiggle_trace_increment=4, pclip=97, cmap=:viridis, style="overlay")
+        f, ax = seisPlotTX(d, dx=dx, dy=dy, xcur=2, wiggle_trace_increment=4, pclip=97, cmap=:viridis, style="overlay")
         @test true
     end
 
     @testset "SeisPlotFK" begin
-        f, ax = SeisPlotFK(d, dx=dx, dy=dy, pclip=97, fmax=90)
+        f, ax = seisPlotFK(d, dx=dx, dy=dy, pclip=97, fmax=90)
 
         @test true
     end
 
     @testset "SeisAmplitude" begin
-        f, ax = SeisPlotAmplitude(d, dy=dy, fmax=90)
+        f, ax = seisPlotAmplitude(d, dy=dy, fmax=90)
 
         @test true
+    end
+
+    @testset "SeisDifference" begin
+        
+        f, ax1, ax2, ax3 = seisDifference(d, d .+ randn())
+
+        @test true
+
     end
 
 end

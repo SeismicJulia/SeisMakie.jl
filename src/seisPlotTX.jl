767c8a4a0a53a74c39acfa949b31fa15c86990a1
AtilaSaraiva <atilasaraiva@gmail.com>
2024-11-17 18:50:42 -0700

    adding support for transparency to seisPlotTX.jl

--- src/seisPlotTX.jl
+++ src/seisPlotTX.jl
@@ -11,7 +11,7 @@ Plot time-space, 2D seismic data `d` with image, wiggles or overlay.
 # Keyword Arguments:
 - `fig=nothing`: the figure we want to plot on. If not supplied, one will be created and returned.
 
-- `gx::Vector{<:Real}=nothing`: the real coordinates of the seismometers corresponding to the traces in d. Only 
+- `gx::Vector{<:Real}=nothing`: the real coordinates of the seismometers corresponding to the traces in d. Only
                                 supported with style="wiggles"
 - `ox=0`: first point of x-axis.
 - `dx=1`: increment of x-axis.
@@ -45,7 +45,7 @@ function seisPlotTX(d;
                     fig=nothing, ax=nothing, gx=nothing, ox=0, dx=1, oy=0, dy=1, xcur=1.2, wiggle_trace_increment=1,
                     pclip=98, vmin=nothing, vmax=nothing,
                     wiggle_line_color=:black, wiggle_fill_color=:black, trace_width=0.7,
-                    cmap=:seismic, style="image")
+                    cmap=:seismic, style="image", alpha::AbstractFloat=1.0, transparency=false)
 
     if isnothing(fig)
         fig = Figure()
@@ -61,7 +61,8 @@ function seisPlotTX(d;
                                wiggle_trace_increment=wiggle_trace_increment,
                                wiggle_line_color=wiggle_line_color,
                                wiggle_fill_color=wiggle_fill_color,
-                               trace_width=trace_width, cmap=cmap)
+                               trace_width=trace_width, cmap=cmap,
+                               transparency=false, alpha=1.0)
 
         Colorbar(fig[1, 2], overlay)
 
@@ -85,7 +86,8 @@ function seisPlotTX(d;
         xlims!(ax, low=start-diff, high=start + size(to_value(d), 2)*diff)
 
     else
-        img = seisimageplot!(ax, d; ox=ox, dx=dx, oy=oy, dy=dy, pclip=pclip, vmin=vmin, vmax=vmax, cmap=cmap)
+        img = seisimageplot!(ax, d; ox=ox, dx=dx, oy=oy, dy=dy, pclip=pclip, vmin=vmin, vmax=vmax,
+                             cmap=cmap, alpha=alpha, transparency=transparency)
         Colorbar(fig[1,2], img)
 
         xlims!(ax, low=to_value(ox), high=to_value(ox)+size(to_value(d),2)*to_value(dx))
